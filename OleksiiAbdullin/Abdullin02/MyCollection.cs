76cd883063b3bf15e809f446606a2b14c985ebc7
AbdullinAlex <drftg4@gmail.com>
2021-11-23 15:37:24 +0200

    Lab02 is done

---
+++ OleksiiAbdullin/Abdullin02/MyCollection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Abdullin02
+{
+    public class MyCollection : IEnumerable
+    {
+        private List<Student> _studentsArray = new List<Student>();
+
+        public void Add(Student student)
+        {
+            if(student is null)
+            {
+                student = new Student();
+            }
+            _studentsArray.Add(student);
+        }
+
+        public Student GetStudent(Student student)
+        {
+            foreach (var stud in _studentsArray)
+            {
+                if (stud.Equals(student))
+                {
+                    return student;
+                }
+            }
+            return null;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        public MyCollectionEnum GetEnumerator()
+        {
+            return new MyCollectionEnum(_studentsArray);
+        }
+    }
+}
--- OleksiiAbdullin/Abdullin02/Program.cs
+++ OleksiiAbdullin/Abdullin02/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Abdullin02
 {
@@ -10,6 +9,55 @@ namespace Abdullin02
     {
         static void Main(string[] args)
         {
+            var studentArray = new Student[3];
+            Student stud = null;
+
+            for (int i = 0; studentArray.Length > i; i++)
+            {
+                stud = new Student();
+                studentArray[i] = stud;
+            }
+            stud = new Student("Fayana", "Rucus", "b", "KS", 121, 78, new DateTime(2006, 07, 11), new DateTime(2021, 08, 11));
+
+            var MyCollection = new MyCollection();
+
+            foreach (var s in studentArray)
+            {
+                MyCollection.Add(s);
+            }
+
+            Console.WriteLine("Students list:");
+            int id = 0;
+            foreach (var s in MyCollection)
+            {
+                Console.WriteLine("Student ID: " + id++);
+                s.Print();
+            }
+            Console.WriteLine("----------------");
+
+            MyCollection.Add(stud);
+
+            Console.WriteLine("Students list:");
+            id = 0;
+            foreach (var s in MyCollection)
+            {
+                Console.WriteLine("Student ID: " + id++);
+                s.Print();
+            }
+            Console.WriteLine("----------------");
+
+            Student result = MyCollection.GetStudent(stud);
+            if (result == null)
+            {
+                Console.WriteLine("Student has not been found.");
+            }
+            else
+            {
+                Console.WriteLine("Student has been found.\n");
+                result.Print();
+            }
+            Console.WriteLine("----------------");
+            Console.ReadKey();
         }
     }
 }
