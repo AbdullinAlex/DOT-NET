424075cf15adea6cec8eaa0fee3ce3c108160f09
AbdullinAlex <drftg4@gmail.com>
2021-12-06 15:17:28 +0200

    Lab08 is done.

---
+++ OleksiiAbdullin/Abdullin08/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Abdullin08.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
